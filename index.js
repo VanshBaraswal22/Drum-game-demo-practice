73c2ddcf515c42e26724ce6ec372533309de7f21
VanshBaraswal22 <vanshbaraswal22@gmail.com>
2026-09-01 19:34:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector("button").addEventListener("click", handleClick);
+// IT FINDS FIRST BUTTON AND ADDS AN EVENT LISTENER TO IT. WHEN THE BUTTON IS CLICKED, IT WILL CALL THE FUNCTION handleClick.
+function handleClick() {
+    alert("I got clicked");
+}
