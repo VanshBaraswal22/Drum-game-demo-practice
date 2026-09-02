c684b28daad8bfa247d25a9b1bed95ba5daab556
VanshBaraswal22 <vanshbaraswal22@gmail.com>
2026-09-02 19:26:18 +0530

    Add keyboard drum controls and animation

--- index.js
+++ index.js
@@ -1,57 +1,109 @@
-var length1 = document.querySelectorAll(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i=0; i<length1; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-    function handleClick() {
-       // alert("I got clicked");
-       //this.style.color = "white";
-       var buttonInnerHTML = this.innerHTML;
-       switch (buttonInnerHTML) {
-        case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
-        case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-        case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
-        case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-        case "j":
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-        case "k":
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-        case "l":
-            var kick = new Audio("sounds/kick.mp3");
-            kick.play();
-            break;
-        default: console.log(buttonInnerHTML);
+// It finds all elements with the class "drum" and gets their total number.
 
-        // default is added to handle any unexpected input. If the buttonInnerHTML does not match any of the specified cases, it will log the value to the console for debugging purposes.
-       }
-    }
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    // "this" refers to the button that was clicked.
+    // innerHTML gets the text/key inside that button.
+
+    makeSound(buttonInnerHTML);
+
+    // Calls makeSound() and passes the clicked button's key.
+
+    buttonAnimation(buttonInnerHTML);
+
+    // Calls buttonAnimation() to add a visual effect to the clicked button.
+
+  });
+
 }
- //var audio = new Audio("sounds/tom-1.mp3");
-  //      audio.play();
+
+// Adds a keyboard event listener to the document.
+// It runs whenever a key is pressed.
+
+document.addEventListener("keypress", function(event) {
+
+  // event.key gives us the key that was pressed.
+
+  makeSound(event.key);
+
+  // Plays the sound corresponding to the pressed key.
+
+  buttonAnimation(event.key);
+
+  // Animates the corresponding drum button.
+
+});
 
 
+// makeSound() takes a key as an argument and plays the matching sound.
 
-// IT FINDS FIRST BUTTON AND ADDS AN EVENT LISTENER TO IT. WHEN THE BUTTON IS CLICKED, IT WILL CALL THE FUNCTION handleClick.
-// we didnt use handleClick() because we dont want to call the function immediately, we want to pass the function as a reference so that it can be called later when the event occurs.
+function makeSound(key) {
 
-// we could use anonymous function instead of handleClick.
-/* 
-document.querySelector("button").addEventListener("click", function() {
-    alert("I got clicked");
-}); 
-*/
+  // switch checks the key and executes the matching case.
+
+  switch (key) {
+
+    // Each case creates and plays the corresponding audio file.
+
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    // "break" stops the switch after the matching case is executed.
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    // default handles unexpected keys.
+    default: console.log(key);
+  }
+}
+
+
+// Adds a "pressed" class to the button corresponding to the key.
+// After 100ms, the class is removed to create the button animation.
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
